fe87db9486e05340e61a202919219e462463e01a
Jason.wu <wuxingsheng@youdustory.com>
2018-07-18 14:55:38 +0800

    save

--- src/pages/recorder/index.vue
+++ src/pages/recorder/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    ddsafadsfdas
+    <button @tap="start">start record</button>
+    <button @tap="play(tempFilePath)">play record</button>
+  
   </div>
 </template>
 
@@ -8,23 +10,19 @@
 import { formatTime } from "@/utils/index";
 
 export default {
-  components: {},
+ 
 
   data() {
     return {
-      logs: []
+      tempFilePath: "",
     };
   },
 
   mounted() {
-    this.init();
+    
   },
   methods: {
-    init2(){
-      let url = "http://tmp/wx96d1bf9498134556.o6zAJs_dEnSCQzHuWQQ2…92d044cf0c2555cb2fb554db39.durationTime=10004.mp3";
-      this.play(url);
-
-    },
+ 
     play(tempFilePath){
       const innerAudioContext = wx.createInnerAudioContext();
         innerAudioContext.autoplay = true;
@@ -43,7 +41,7 @@ export default {
         
         innerAudioContext.play();
     },
-    init() {
+    start() {//开始录制
       const recorderManager = wx.getRecorderManager();
 
       recorderManager.onStart(() => {
@@ -53,31 +51,16 @@ export default {
         console.log("recorder pause");
       });
       recorderManager.onStop(res => {
-
-        console.log("recorder stop", res);
+ 
         const { tempFilePath } = res;
-         const innerAudioContext = wx.createInnerAudioContext();
-        innerAudioContext.autoplay = true;
-        innerAudioContext.src = tempFilePath;
-        innerAudioContext.onPlay(() => {
-          console.log("开始播放",tempFilePath);
-        });
-        innerAudioContext.onError(res => {
-          console.log(res.errMsg);
-          console.log(res.errCode);
-        });
-        innerAudioContext.onStop(res => {
-          console.log("停止播放");
-        
-        });
-        
-        innerAudioContext.play();
+        this.tempFilePath = tempFilePath;
+         
 
         
       });
       recorderManager.onFrameRecorded(res => {
         const { frameBuffer } = res;
-        console.log("frameBuffer.byteLength", frameBuffer.byteLength);
+       
       });
 
       const options = {
@@ -85,7 +68,7 @@ export default {
         sampleRate: 44100,
         numberOfChannels: 1,
         encodeBitRate: 192000,
-        format: "mp4",
+        format: "m4a",
         frameSize: 50
       };
 
